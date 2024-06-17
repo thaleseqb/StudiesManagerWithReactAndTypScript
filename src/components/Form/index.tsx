a095b6318a1893fb0fa9eba7cb7a48b63f7196ed
thales <thalesquadros2@gmail.com>
2024-06-17 16:00:25 -0300

    install uuid dependency and update the task

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../Button";
 import style from "./Form.module.scss"
 import ITask from "../../types/task";
+import { v4 as uuidv4 } from "uuid";
 
 class Form extends React.Component<{
     setTasks: React.Dispatch<React.SetStateAction<Array<ITask>>>
@@ -13,7 +14,21 @@ class Form extends React.Component<{
 
     addTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        this.props.setTasks((olderTasks) => [...olderTasks, {...this.state}]);
+        this.props.setTasks((olderTasks) => 
+        [
+            ...olderTasks, 
+            {
+                ...this.state, 
+                selected: false,
+                completed:false,
+                id : uuidv4()
+            }
+        ]
+    );
+        this.setState({
+            task:"",
+            time:"00:00"
+        })
     }
 
     render() {
