30016eecc04f87291e2a6c845059494aa5b46c2c
thales <thalesquadros2@gmail.com>
2024-06-17 14:45:29 -0300

    learns about how to control forms using react and statefull components

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,15 +3,27 @@ import Button from "../Button";
 import style from "./Form.module.scss"
 
 class Form extends React.Component {
+    state = {
+        task:"",
+        time:"00:00"
+    }
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log("state", this.state);
+    }
+
     render() {
         return (
-            <form className = {style.novaTarefa}>
+            <form className = {style.novaTarefa} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">
                         Adicine um novo estudo
                     </label>
                     <input type="text"
                     name="task"
+                    value={this.state.task}
+                    onChange={event => this.setState({...this.state , task: event.target.value})}
                     id="task"
                     placeholder="O que você deseja estudar" 
                     required/>
@@ -23,6 +35,8 @@ class Form extends React.Component {
                     <input type="time"
                     step="1"
                     name="time"
+                    value={this.state.time}
+                    onChange={event => this.setState({...this.state, time: event.target.value})}
                     id="time"
                     min="00:00:00"
                     max="00:01:30"
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./List.module.scss"
 import Item from "./Item"
 
 function List() {
 
-    const tasks = [{
+    const [tasks, setTasks] = useState([{
         task: "React",
         time: "02:00:00"
     },{
@@ -13,11 +13,13 @@ function List() {
     },{
         task: "TypeScript",
         time: "03:00:00"
-    }]
+    }]);
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks, {task: "Estudar estado", time: "05:00:00"} ]);
+            }}>Estudos do dia</h2>
             <ul>
                 {tasks.map( (item, index) => (
                         <Item
